ec607c42e679ee5dab74999636c5cdf298a655af
carolinaltorres <carolinadelimatorres@gmail.com>
2022-03-10 22:48:57 -0300

    @carolinaltorres [Task: creating test]

--- src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,15 +1,19 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,16 +25,31 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @PostMapping ("/logar")
     public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
-        return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+        return usuarioService.Logar(user)
+        	.map(resp -> ResponseEntity.ok(resp))
             .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
     @PostMapping ("/cadastrar")
     public ResponseEntity<Usuario> Post (@RequestBody Usuario usuario) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(usuarioService.CadastrarUsuario(usuario));
+        return usuarioService.CadastrarUsuario(usuario)
+            	.map(resp -> ResponseEntity.status(201).body(resp))
+                .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<Usuario>> GetAll() {
+    	return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+    
+    @PutMapping("/atualizar")
+    public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
+    	return ResponseEntity.ok(usuarioRepository.save(usuario));
     }
 }
--- src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -21,6 +21,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+        
+        auth.inMemoryAuthentication().withUser("carolina").password(passwordEncoder().encode("carolina")).authorities("ADMIN");
     }
     
     @Bean
